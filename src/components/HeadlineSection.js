5c3b5b5c08682980f96b536c9a89432c563d304d
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-30 11:17:20 +0100

    html components

---
+++ src/components/HeadlineSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function headlineSection() {
+  return (
+    <div className="HeadlineSection">
+      <img src={require("../images/image-web-3-desktop.jpg")} />
+      <div>
+        <h1>The Bright Future of Web 3.0?</h1>
+      </div>
+      <div>
+        <p>
+          We dive into the next evolution of the web that claims to put the
+          power of the platforms back into the hands of the people. But is it
+          really fulfilling its promise?
+        </p>
+        <button>Read more</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/PopularSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function popularSection() {
+  return (
+    <div className="PopularSection">
+      <div>
+        <img src={require("../images/image-retro-pcs.jpg")} />
+        <div>01</div>
+        <h4>Hydrogen VS Electric Cars</h4>
+        <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+      </div>
+      <div>
+        <img src={require("../images/image-top-laptops.jpg")} />
+        <div>02</div>
+        <h4>The Downsides of AI Artistry</h4>
+        <p>
+          What are the possible adverse effects of on-demand AI image
+          generation?
+        </p>
+      </div>
+      <div>
+        <img src={require("../images/image-gaming-growth.jpg")} />
+        <div>03</div>
+        <h4>Is VC Funding Drying Up?</h4>
+        <p>
+          Private funding by VC firms is down 50% YOY. We take a look at what
+          that means.
+        </p>
+      </div>
+    </div>
+  );
+}
